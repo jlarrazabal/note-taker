76369cb029bd262793edff4b9374ee752e74352f
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-05-13 00:03:48 -0400

    Adding function to handle post request to save new notes.
    Adding function to handle post request to save new notes.

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 //Npm Packages
 const express = require("express");
-const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require('uuid'); //uuidv4(); ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 const PORT = process.env.PORT || 3000; //Includes port for Heroku
 const app = express();
 
+//Classes
+class Note {
+  constructor(title, text) {
+      this.title = title;
+      this.text = text;
+      this.id = uuidv4();
+  }
+}
+
 //Using Statice Files and Body Parser
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -28,7 +37,31 @@ app.get("/api/notes", function(req, res){
 });
 
 app.post("/api/notes", function(req, res) {
-  console.log(req);
+  const newNote = new Note(req.body.title,req.body.text);
+  console.log(newNote);
+  fs.readFile(`${__dirname}/db/db.json`, 'utf8', function(err, data){
+    if(err){
+      throw err;
+    } else {
+      console.log(data);
+      const db = [];
+      JSON.parse(data).forEach((item, i) => {
+        db.push(item);
+      });
+      db.push(newNote);
+      if(db[0].length === 0) {
+        db.shift();
+      }
+      fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(db), function(err){
+        if(err) {
+          throw err;
+        } else {
+          console.log("Note added to data base");
+        }
+      });
+      res.redirect("/api/notes");
+    }
+  });
 });
 
 
