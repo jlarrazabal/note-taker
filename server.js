65971e0031b628f0e28f264c2771dcbf5943d974
jlarrazabal <69408299+jlarrazabal@users.noreply.github.com>
2021-05-13 22:26:19 -0400

    Server.js Code Complete. Includes function to delete notes from the db.json file
    Server.js Code Complete
    - Includes the function to delete the notes from the db.json file.

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-//Npm Packages
+//NPM Packages
 const express = require("express");
 const fs = require("fs");
 const path = require("path");
-const { v4: uuidv4 } = require('uuid'); //uuidv4(); ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
+const { v4: uuidv4 } = require('uuid'); // Package to create unique ids for the note objects.
 const PORT = process.env.PORT || 3000; //Includes port for Heroku
 const app = express();
 
@@ -11,21 +11,21 @@ class Note {
   constructor(title, text) {
       this.title = title;
       this.text = text;
-      this.id = uuidv4();
+      this.id = uuidv4(); //Adds an unique id for the note object when created => Example: "1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed".
   }
 }
 
-//Using Statice Files and Body Parser
+//Using Static Files, Body Parser, and JSON.
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static(path.join(__dirname, "public")));
 
-//Paths
+//Sends the notes.html file when the user hits the "/notes" path.
 app.get("/notes", function(req, res){
   res.sendFile(`${__dirname}/public/notes.html`);
 });
 
+//Sends the information in the db.json file.
 app.get("/api/notes", function(req, res){
   fs.readFile(`${__dirname}/db/db.json`,function(err, data){
     if(err){
@@ -36,6 +36,7 @@ app.get("/api/notes", function(req, res){
   });
 });
 
+//Captures The note save event, reads the db.json file, adds the new note, updates the db.json file, and redirects the user to "/notes".
 app.post("/api/notes", function(req, res) {
   const newNote = new Note(req.body.title,req.body.text);
   console.log(newNote);
@@ -64,13 +65,41 @@ app.post("/api/notes", function(req, res) {
   });
 });
 
+//Captures The note delete event, reads the db.json file, deletes the note selected by de user by the note id, updates the db.json file, and redirects the user to "/notes".
+app.delete("/api/notes/:id", function(req, res) {
+  const id = req.params.id;
+  fs.readFile(`${__dirname}/db/db.json`, 'utf8', function(err, data){
+    if(err){
+      throw err;
+    } else {
+      console.log(data);
+      const db = [];
+      JSON.parse(data).forEach((item, i) => {
+        db.push(item);
+      });
+      db.forEach((item, i) => {
+        if(item.id === id) {
+          db.splice(i, 1);
+        }
+      });
+      fs.writeFile(`${__dirname}/db/db.json`, JSON.stringify(db), function(err){
+        if(err) {
+          throw err;
+        } else {
+          console.log("Note added to data base");
+        }
+      });
+      res.redirect("/api/notes");
+    }
+  });
+});
 
-
+//Sends the index.html file for any route that is note specified in server.
 app.get("*",function(req, res){
   res.sendFile(`${__dirname}/public/index.html`);
 });
 
-
+//listens process.env.PORT and port 3000 for interactions with the server.
 app.listen(PORT, function() {
   console.log(`Server is running in port ${PORT}!`);
 });
